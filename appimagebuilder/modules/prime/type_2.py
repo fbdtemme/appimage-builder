af66a6c1d50bcdefbc2e9a2e9255e8b77cffdaa5
Alexis Lopez Zubieta <contact@azubieta.net>
2021-07-05 11:52:39 -0500

    Use format entry from AppImage section in recipe

--- appimagebuilder/modules/prime/type_2.py
+++ appimagebuilder/modules/prime/type_2.py
@@ -29,12 +29,24 @@ from appimagebuilder.gateways.appimagetool import AppImageToolCommand
 
 
 class Type2Creator:
-    def __init__(self, recipe):
-        self.app_dir = recipe.AppDir.path()
-        self.target_arch = recipe.AppImage.arch()
-        self.app_name = recipe.AppDir.app_info.name()
-        self.app_version = recipe.AppDir.app_info.version()
-        self.update_information = recipe.AppImage["update-information"]() or "None"
+    def __init__(
+        self,
+        app_dir,
+        app_name,
+        app_version,
+        target_arch,
+        update_information,
+        sign_key,
+        file_name,
+    ):
+        self.app_dir = app_dir
+        self.app_name = app_name
+        self.app_version = app_version
+        self.target_arch = target_arch
+        self.update_information = update_information
+        self.sign_key = sign_key
+        self.file_name = file_name
+
         self.guess_update_information = False
 
         if self.update_information == "None":
@@ -46,15 +58,11 @@ class Type2Creator:
             self.update_information = None
             self.guess_update_information = True
 
-        self.sing_key = recipe.AppImage["sign-key"] or "None"
-        if self.sing_key == "None":
-            self.sing_key = None
-
         fallback_file_name = os.path.join(
             os.getcwd(),
             "%s-%s-%s.AppImage" % (self.app_name, self.app_version, self.target_arch),
         )
-        self.target_file = recipe.AppImage.file_name or fallback_file_name
+        self.target_file = self.file_name or fallback_file_name
 
     def create(self):
         self._assert_target_architecture()
@@ -70,7 +78,7 @@ class Type2Creator:
         appimage_tool.target_arch = self.target_arch
         appimage_tool.update_information = self.update_information
         appimage_tool.guess_update_information = self.guess_update_information
-        appimage_tool.sign_key = self.sing_key
+        appimage_tool.sign_key = self.sign_key
         appimage_tool.runtime_file = runtime_path
         appimage_tool.run()
 
